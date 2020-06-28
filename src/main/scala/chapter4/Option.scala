f32e805a6be556d081cbdab0862ec0e12cfccbea
Alan Bayley <alan_bayley@hotmail.co.uk>
2020-06-28 21:46:44 +0100

    * completed exercise 4.1

--- src/main/scala/chapter4/Option.scala
+++ src/main/scala/chapter4/Option.scala
@@ -2,36 +2,24 @@ package chapter4
 
 sealed trait Option[+A] {
 
-  def map[B](f: A => B): Option[B]
-  def flatMap[B](f: A => Option[B]): Option[B]
-  def getOrElse[B >: A](default: => B): B
-  def orElse[B >: A](ob: => Option[B]): Option[B]
-  def filter(f: A => Boolean): Option[A]
+  def map[B](f: A => B): Option[B] = this match {
+    case Some(value) => Some(f(value))
+    case None => None
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] = this map f getOrElse None
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case Some(value) => value
+    case None => default
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = if (this == None) ob else this
+
+  def filter(f: A => Boolean): Option[A] = if (this map f getOrElse false) this else None
 
 }
 
-case class Some[+A](get: A) extends Option[A] {
+case class Some[+A](get: A) extends Option[A]
 
-  def map[B](f: A => B): Option[B] = ???
-
-  def flatMap[B](f: A => Option[B]): Option[B] = ???
-
-  def getOrElse[B >: A](default: => B): B = ???
-
-  def orElse[B >: A](ob: => Option[B]): Option[B] = ???
-
-  def filter(f: A => Boolean): Option[A] = ???
-
-}
-
-case object None extends Option[Nothing] {
-  override def map[B](f: Nothing => B): Option[B] = None
-
-  override def flatMap[B](f: Nothing => Option[B]): Option[B] = None
-
-  override def getOrElse[B >: Nothing](default: => B): B = default
-
-  override def orElse[B >: Nothing](ob: => Option[B]): Option[B] = None
-
-  override def filter(f: Nothing => Boolean): Option[Nothing] = None
-}
+case object None extends Option[Nothing]
--- src/test/scala/chapter4/Exercise4p1Spec.scala
+++ src/test/scala/chapter4/Exercise4p1Spec.scala
@@ -4,40 +4,61 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class Exercise4p1Spec extends FlatSpec with Matchers {
 
+  private val some = Some(1)
+  private val none = None
+
   behavior of "map"
 
-  it should "apply the transform to each element" in {
+  it should "apply the transform to a non-empty option" in {
+    some map( _*2 ) shouldEqual Some(2)
+  }
 
+  it should "ignore the transform for an empty option" in {
+    none map( x => x ) shouldEqual None
   }
 
   behavior of "flatMap"
 
-  it should "apply the transform to each element and return a flattened list" in {
-
+  it should "apply the transform to an non-empty option" in {
+    some flatMap( x => Some(x*2) ) shouldEqual Some(2)
   }
 
-  it should "remove transformation results of None" in {
-
+  it should "ignore the transform for an empty option" in {
+    none flatMap( _ => Some(3) ) shouldEqual None
   }
 
   behavior of "getOrElse"
 
-  it should "return the value if it exists" in {
-
+  it should "return the option value for a non-empty option" in {
+    some getOrElse 3 shouldEqual 1
   }
 
-  it should "return None if the value doesn't exist" in {
-
+  it should "return the default value for an empty option" in {
+    none getOrElse 3 shouldEqual 3
   }
 
   behavior of "orElse"
 
-  it should "return the first option if it's defined" in {
-
+  it should "return the option for a non-empty option" in {
+    some orElse Some(3) shouldEqual Some(1)
   }
 
-  it should "return the second option if the first is not defined" in {
+  it should "return the default option for an empty option" in {
+    none orElse Some(3) shouldEqual Some(3)
+  }
 
+  behavior of "filter"
+
+  it should "return the option if the predicate is satisfied" in {
+    some filter( _ == 1 ) shouldEqual Some(1)
+  }
+
+  it should "return an empty option if the predicate is not satisfied" in {
+    some filter( _ == 3 ) shouldEqual None
+  }
+
+  it should "return an empty option if the option is empty" in {
+    none filter( _ == 3 ) shouldEqual None
   }
 
 }
